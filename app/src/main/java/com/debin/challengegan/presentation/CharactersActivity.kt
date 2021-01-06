98941fc63718b7ef71f4167cac063dbc3f73d4af
2020-DEV-133 <debintomdt@gmail.com>
2021-01-06 21:47:29 +0000

    layouts added

--- app/src/main/java/com/debin/challengegan/presentation/CharactersActivity.kt
+++ app/src/main/java/com/debin/challengegan/presentation/CharactersActivity.kt
@@ -2,11 +2,22 @@ package com.debin.challengegan.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.debin.challengegan.R
 
 class CharactersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_characters)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this,
+            R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
+    }
+
+
 }
